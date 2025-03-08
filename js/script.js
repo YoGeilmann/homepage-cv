0d55f54bd4f4c30a02c6687592053ed2d45b016d
Josef Springer <snipes@MacBook-Pro-von-Josef.local>
2025-03-08 15:25:39 +0100

    main content and styling

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Show loading indicator until the page is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loading").style.display = "none";
+});
